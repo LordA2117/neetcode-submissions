0036ccb2e5f7d9af35dc679986be209319ccdf67
LordA2117 <88305634+LordA2117@users.noreply.github.com>
2026-09-19 11:28:57 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = {}
+        for string in strs:
+            if tuple(sorted(string)) not in hashmap:
+                hashmap[tuple(sorted(string))] = [string]
+            else:
+                hashmap[tuple(sorted(string))].append(string)
+
+        res = []
+        for key, value in hashmap.items():
+            res.append(value)
+
+        return res
+        
